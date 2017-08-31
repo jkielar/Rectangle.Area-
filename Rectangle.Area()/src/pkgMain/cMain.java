8d0217e6df26d7bb8600a0ec90ca54a1feda47eb
johnkielar <johnkielar@wifi-roaming-128-4-91-218.host.udel.edu>
2017-08-31 18:36:15 -0400

    Entered name into cMain

---
+++ Rectangle.Area()/src/pkgMain/cMain.java
@@ -0,0 +1,12 @@
+package pkgMain;
+
+public class cMain {
+
+	public static void main(String[] args) {
+		 // My GitHub account name is: <jkielar> 
+		 
+		Rectangle rec = new Rectangle(1.5, 2.5);		
+		System.out.println("Rectangle Area: " + rec.Area());
+	}
+
+}
